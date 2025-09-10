f5f8ca72116044eb0733b3a476dba857c057a0ab
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-09-10 18:50:45 +0200

    add endpoint to serve posts data

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+import { posts } from './data/posts'
+
+app.use('/bacheca', (req, res) => {
+    res.json(posts);
+})
+
 app.use('/', (req, res) => {
     res.send('Benvenuto sul mio server scritto in TypeScript!');
 });
